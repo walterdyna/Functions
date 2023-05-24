3f8226f4f42ba43c4554dd9c0d4614cb342181f6
walterdyna <alexdyna@gmail.com>
2023-05-24 19:22:15 -0300

    Function

---
+++ Function.js
@@ -0,0 +1,17 @@
+/*
+----------------------------------- Function -----------------------------------
+
+Trecho de código que é executado
+SOMENTE  quando solicitado (parente Gente Boa !!!)
+
+*/
+
+//----------------------------------- Modo Normal -----------------------------------
+
+const variavel = "abacatinho"
+
+console.log (variavel)
+
+//----------------------------------- Function -----------------------------------
+//So e executado quando e chamada
+
